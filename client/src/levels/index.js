82ed0fee19a3bb458c06987e23ec55696bcbbe10
David Jungst <djungst@gmail.com>
2018-09-10 17:56:26 -0600

    move levels back to non dev status

--- client/src/levels/index.js
+++ client/src/levels/index.js
@@ -1,14 +1,14 @@
 const levels = {
   1: {
-    objective: { text: 'Drink 10 beers', beers: 1 },
+    objective: { text: 'Drink 10 beers', beers: 10 },
     mechanics: [],
   },
   2: {
-    objective: { text: 'Drink 25 beers', beers: 2 },
+    objective: { text: 'Drink 25 beers', beers: 20 },
     mechanics: ['TimedPowerUp'],
   },
   3: { 
-    objective: { text: 'Drink 50 beers', beers: 3 },
+    objective: { text: 'Drink 50 beers', beers: 30 },
     mechanics: ["Boulder"],
   }
 }
